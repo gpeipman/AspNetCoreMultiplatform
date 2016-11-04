a9697aeb914a4a7c836ff5a2cc299e3e5457d376
Gunnar <gpeipman@hotmail.com>
2016-11-04 10:20:01 +0200

    Default web application with small code clean-up

---
+++ src/AspNetCoreMultiplatform/Models/ManageViewModels/AddPhoneNumberViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreMultiplatform.Models.ManageViewModels
+{
+    public class AddPhoneNumberViewModel
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ src/AspNetCoreMultiplatform/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AspNetCoreMultiplatform.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
